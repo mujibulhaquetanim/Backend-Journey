1fb245941d5a9f7398fef2283666ddce13df834c
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-01-05 21:39:34 +0600

    Auth project structure added

---
+++ DB Models/MongoDB.js
@@ -0,0 +1,6 @@
+//creating schema.
+//creating new instances for the schema.
+//connecting Mongodb with NODEjs.
+//creating middleware
+//applying CRUD operations.
+//creating routes for different APIs.
