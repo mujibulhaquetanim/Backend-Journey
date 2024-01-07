8fe56f8ffc25b3d5f325035b2e06bd561d6523f7
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-01-07 19:52:55 +0600

    project structure initiated

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 const app = express();
+
+const port = process.env.PORT || 3000;
+
 app.get('/', (req,res)=>{
     res.send('Server is Ready')
 });
-const port = process.env.PORT || 3000;
 
 app.listen(port,()=>{
     console.log(`server at http://localhost:${port}`);
