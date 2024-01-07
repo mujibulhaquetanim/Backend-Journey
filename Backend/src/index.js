ddb6c002deffdb69ba7c869b0b6239a92b73d2b3
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-01-07 23:53:54 +0600

    comment added

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 //const fetch= require(src/api/fetch)
 const app = express();
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000; //process.env.PORT not working directly
 
 app.get('/', (req,res)=>{
     res.send('Server is Ready')
