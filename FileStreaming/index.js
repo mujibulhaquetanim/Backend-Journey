1e1e73fc7847c0b511124bc4e794b8e8b56274d4
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-02-24 23:58:13 +0600

    express-status-monitor installed

--- FileStreaming/index.js
+++ FileStreaming/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { normalRead } = require('./fileRead');
+const status= require("express-status-monitor");
 
 const app = express();
 const PORT = 8000;
 
+app.use(status());
 app.get('/',(req,res)=>{
     res.send("try /normal");
 });
