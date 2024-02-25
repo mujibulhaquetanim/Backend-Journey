36e9ceaa9eb498c5b8d99dd1e11f13864517cd00
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-02-25 19:10:27 +0600

    /stream end point and its logic added.

--- FileStreaming/fileRead.js
+++ FileStreaming/fileRead.js
@@ -1,9 +1,15 @@
 const fs = require("fs");
 
-const normalRead = ((_,res) => {
+const normalRead = ((_, res) => {
     fs.readFile("./public/file.txt", (err, data) => {
         res.end(data);
     })
 });
 
-module.exports = { normalRead }
+const stream = ((_, res) => {
+    const stream = fs.createReadStream("./public/file.txt", "utf-8");
+    stream.on("data", (chunk) => res.write(chunk));
+    stream.on("end", () => res.end());
+})
+
+module.exports = { normalRead, stream };
--- FileStreaming/index.js
+++ FileStreaming/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const { normalRead } = require('./fileRead');
-const status= require("express-status-monitor");
+const { normalRead, stream } = require('./fileRead');
+const status = require("express-status-monitor");
 
 const app = express();
 const PORT = 8000;
 
 app.use(status());
-app.get('/',(req,res)=>{
-    res.send("try /normal");
+app.get('/', (req, res) => {
+    res.send("try '/normal' for normal data read\ntry '/stream' for stream read");
 });
 
-app.get('/normal',normalRead);
+app.get('/normal', normalRead);
+app.get('/stream', stream);
 
 app.listen(PORT, () => {
     console.log('listening on port ' + PORT);
