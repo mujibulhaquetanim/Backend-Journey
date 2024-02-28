ff0f5eb99a45f88c04b831e291d49d9495004fbd
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-02-28 23:58:50 +0600

    Changed the import of the 'cluster' module from 'cluster' to 'node:cluster'. Added logging of the number of CPUs. If the process is the primary process, it forks the number of CPUs minus one worker processes. If the process is a worker process, it logs its process ID.

--- FileHandlingStreaming/api/nodeCluster.js
+++ FileHandlingStreaming/api/nodeCluster.js
@@ -1,6 +1,13 @@
-const cluster = require('cluster');
+const cluster = require('node:cluster');
 const os = require('os');
 
 const numCPUs = os.cpus().length;
-
 console.log('numCPUs: ', numCPUs);
+
+if(cluster.isPrimary){
+    for(let i=0;i<numCPUs;i++){
+        cluster.fork();
+    }
+}else{
+    console.log(process.pid);
+}
