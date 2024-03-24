a294c0030946504c78ff72edc6c599c93ae5c6a5
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-03-24 23:05:42 +0600

    work_thread and execFile added

--- FileHandlingStreaming/api/childProcess.js
+++ FileHandlingStreaming/api/childProcess.js
@@ -1,7 +1,7 @@
-const { exec } = require('child_process');
+const { exec, execFile } = require('child_process');
 
 const exe = (_, res) => {
-    exec('dir', (error, stdout, stderr) => {
+    exec('ls', (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
             return;
@@ -10,12 +10,27 @@ const exe = (_, res) => {
             console.log(`stderr: ${stderr}`);
             return;
         }
-        res.send(`${stderr}`);
+        res.end(`${stderr}`);
         console.log(`stdout: ${stdout}`);
     });
-
-
 }
-console.log(exe)
 
-module.exports = { exe };
+const exeFile = (_, res) =>{
+    execFile('../public/script.sh', (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        res.end(`${stderr}`);
+        console.log(`stdout: ${stdout}`);
+})
+}
+
+
+    console.log(exe)
+
+module.exports = { exe, exeFile }
---
+++ FileHandlingStreaming/public/script.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+
+pwd
+echo "Hello from the other side"
