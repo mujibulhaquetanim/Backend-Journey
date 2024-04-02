e8cf6c01a01eecee173ef02f3dc0083ff0c8e6bf
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-04-02 23:54:31 +0600

    userSchema for userdata Database created.

---
+++ Authentication/server/models/userSchema.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    phone: {
+        type: String,
+        require: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+});
+
+export const User = new mongoose.model("User", userSchema);
