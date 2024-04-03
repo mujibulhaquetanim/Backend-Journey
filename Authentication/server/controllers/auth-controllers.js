770a9934d58b5ddc955fb042f6d92e2145e54726
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-04-03 22:58:18 +0600

    register controller added

--- Authentication/server/controllers/auth-controllers.js
+++ Authentication/server/controllers/auth-controllers.js
@@ -1,24 +1,36 @@
+import { User } from "../models/userSchema.model.js";
+
 const home = async (req, res) => {
     try {
-        res.status(200).json({message:'Welcome to the User Auth page'});
+        res.status(200).json({ message: 'Welcome to the User Auth page' });
     } catch (error) {
-        res.status(400).json({message:'Page not found: ' + error.message});
+        res.status(400).json({ message: 'Page not found: ' + error.message });
     }
-}
+};
 
 const register = async (req, res) => {
     try {
-        res.status(200).json({message:'Welcome to the User Register page' + req.body});
+        const { username, email, password, phone } = req.body;
+
+        const userExist = await User.findOne({ email });
+        if (userExist) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
+        await User.create({ username, email, password, phone });
+
+        res.status(200).json({ message: `new user name: ${username} created successfully` });
     } catch (error) {
-        res.status(400).json({message:'Page not found: ' + error.message});
+        res.status(400).json({ message: 'Page not found: ' + error.message });
     }
-}
+};
+
 const login = async (req, res) => {
     try {
-        res.status(200).json({message:'Welcome to the User Login page' + req.body});
+        res.status(200).json({ message: 'Welcome to the User Login page' + req.body });
     } catch (error) {
-        res.status(400).json({message:'Page not found: ' + error.message});
+        res.status(400).json({ message: 'Page not found: ' + error.message });
     }
-}
+};
 
 export { home, register, login }
