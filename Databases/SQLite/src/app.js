c1bc4e76610a12bfd2cfd9f6811f2ec348f43055
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-05-03 23:59:10 +0600

    ; added in query table & tryCatch added for error handling

--- Databases/SQLite/src/app.js
+++ Databases/SQLite/src/app.js
@@ -1,11 +1,19 @@
 import Database from 'better-sqlite3';
 
-const db = new Database('app.db');
+try {
+    const db = new Database('app.db');
 
-const query = `
+    const query = `
     CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        username STRING NOT NULL UNIQUE`
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      username TEXT NOT NULL UNIQUE
+    )
+  `;
 
-db.exec(query);
+    db.exec(query);
+
+    console.log('Table "users" created successfully (if it didn\'t exist already).');
+} catch (error) {
+    console.error('Error creating table:', error.message);
+}
