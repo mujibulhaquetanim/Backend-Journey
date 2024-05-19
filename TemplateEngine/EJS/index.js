84ad048631d1bb91a8553f08c1b69f9f2e02e938
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-05-19 12:15:06 +0600

    css added and express.static middleware added in index file

--- TemplateEngine/EJS/index.js
+++ TemplateEngine/EJS/index.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = 8000;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 let languages = ["c", "js", "py", "go"];
