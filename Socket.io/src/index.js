ef1f7be86f5f2cadb93f10c035ca2c817db8b6af
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-05-20 23:20:32 +0600

    server created and static page added without an templateEngine

--- Socket.io/src/index.js
+++ Socket.io/src/index.js
@@ -1,11 +1,16 @@
-import express from 'express'
+import http from 'http';
+import express from 'express';
+import path from 'path';
 
 const app = express();
+const server = http.createServer(app);
+
+app.use(express.static(path.resolve("./public")));
 
 app.get('/', (_, res) => {
-    res.end("Welcome to socket io");
+    res.sendFile('/public/index.html');
 });
 
-app.listen(8000, () => {
+server.listen(8000, () => {
     console.log("listening on localhost:8000")
 })
