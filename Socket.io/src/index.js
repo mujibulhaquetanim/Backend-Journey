37d1689f1e221fcf19285872274014650b921261
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-05-21 23:46:18 +0600

    message sent from client to server and broadcasted from server using console.log

--- Socket.io/src/index.js
+++ Socket.io/src/index.js
@@ -1,14 +1,18 @@
 import http from 'http';
 import express from 'express';
 import path from 'path';
-import {Server} from 'socket.io';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-io.on('connection', (socket)=>{
-    console.log('new user has connected',socket.id);
+io.on('connection', (socket) => {
+    // console.log('new user has connected',socket.id);
+    socket.on('usr-msg', (msg) => {
+        // console.log('A new user message:', msg);
+        io.emit('message', msg)
+    })
 })
 
 app.use(express.static(path.resolve("./public")));
