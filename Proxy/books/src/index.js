380a6063d802789fe04e7383684e0d5748abe64c
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-05-29 23:59:43 +0600

    restructured in folders and htt-proxy-middleware added and inited

---
+++ Proxy/books/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.use(
+    '/book-list',
+    createProxyMiddleware({
+      target: 'http://127.0.0.1:3001',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/book-list': '',
+      },
+    }),
+  );
+
+app.get('/', (req, res) => {
+    res.json({ success: true });
+});
+
+app.listen(3000, () => {
+    console.log(`listening on localhost:${PORT}`);
+});
