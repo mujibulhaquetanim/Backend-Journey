d584c4f8ff8f6ab507e4285298331af8b6bda38b
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-05-31 23:33:01 +0600

    writeFileSync added and replaced the older data of db.json file

--- Databases/JsonDB/index.js
+++ Databases/JsonDB/index.js
@@ -2,14 +2,26 @@ const express = require('express');
 const data = require('./db.json');
 const app = express();
 const fs = require('fs');
+const { error } = require('console');
 
+const newData = [{"id":"06",
+"name":"book6",
+"author":"author6",
+"price":600
+}]
 app.get('/', (_, res) => {
     res.send('hello to the mock server');
 })
 
-const newData = [{}]
+app.get('/data', (_, res) => {
+    res.send(data);
 
-fs.writeFile('data',JSON.stringify(newData));
+    fs.writeFileSync('db.json',JSON.stringify(newData),(error)=>{
+        if(error) throw error;
+        console.log("replaced older data with 'newData'");
+    });
+});
+
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
