4ce0c88dfd64ff2bcd82508bba8bddd85ad4027b
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-05-31 22:50:57 +0600

    fakeData & fs module added

--- Databases/JsonDB/index.js
+++ Databases/JsonDB/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
-
+const data = require('./db.json');
 const app = express();
+const fs = require('fs');
 
 app.get('/', (_, res) => {
     res.send('hello to the mock server');
 })
+
+const newData = [{}]
+
+fs.writeFile('data',JSON.stringify(newData));
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
