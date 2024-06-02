73070c4f118edf183de8d384397d9fcddef520b6
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-06-02 22:16:54 +0600

    "Updated debug package to 4.3.5, modified fs require statement in InsertingNewData.js, and added positional inserting in the db.json file"

--- Databases/JsonDB/src/InsertingNewData.js
+++ Databases/JsonDB/src/InsertingNewData.js
@@ -1,13 +1,26 @@
-const fs = require('fs');
+const fs = require('node:fs').promises;
 
-const newData = [{
-    "id": 5,
-    "title": "The Lord of the Rings",
-    "author": "J. R. R. Tolkien",
-    "year": 1954
-}]
+const addFileAtPosition = async function addFileAtPosition(filePath, newFile, position) {
+    try {
+        const data = await fs.readFile(filePath, 'utf8');
 
-const InsertingNewData= fs.writeFile('db.json',JSON.stringify(newData),(error)=>{
-    if(error) console.log('');
-    console.log("replaced older data with 'newData'");
-});
+        const jsonData = JSON.parse(data);
+
+        if (!Array.isArray(jsonData)) {
+            throw new Error('Invalid JSON format: data is not an array.');
+        }
+
+        if (position === -1) {
+            jsonData.push(newFile);
+        } else {
+            jsonData.splice(position, 0, newFile);
+        }
+
+        await fs.writeFile(filePath, JSON.stringify(jsonData, null, 2), 'utf8');
+        console.log('File added successfully.');
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+};
+
+module.exports = { addFileAtPosition };
--- Databases/JsonDB/src/index.js
+++ Databases/JsonDB/src/index.js
@@ -1,18 +1,32 @@
 const express = require('express');
-const data = require('./db.json');
-const { InsertingNewData } = require('./InsertingNewData');
+const path = require('path');
+const { addFileAtPosition } = require('./InsertingNewData');
+const fs = require('node:fs').promises;
+
 const app = express();
 
-app.get('/', (_, res) => {
-    res.send('hello to the mock server');
-})
+const newData = {
+    "id": 5,
+    "title": "The Hobbit",
+    "author": "J. R. R. Tolkien",
+    "year": 1937
+};
 
-app.get('/data', (_, res) => {
-    res.send(data);
-    InsertingNewData()
+app.get('/', (_, res) => {
+    console.log('hello to the mock server');
+    res.send('hello to the mock server');
 });
 
+app.get('/data', async (_, res) => {
+    const filePath = path.join(__dirname, '../db.json');
+
+    await addFileAtPosition(filePath, newData, -1);
+
+    const updatedData = await fs.readFile(filePath, 'utf8');
+
+    res.send(JSON.parse(updatedData));
+});
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
-})
+});
