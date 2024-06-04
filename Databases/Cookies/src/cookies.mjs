84ad9d3ff0af4b3f8a09de29b9d4dda7271b69e4
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-06-04 23:02:48 +0600

    getcookie route added for getting set cookie from the headers

--- Databases/Cookies/src/cookies.mjs
+++ Databases/Cookies/src/cookies.mjs
@@ -10,6 +10,11 @@ app.get('/', (_, res) => {
     res.status(201).send('cookie set');
 });
 
+app.get('/getcookie', (req, res) => {
+    console.log(req.headers.cookie);
+    res.send(req.headers.cookie);
+});
+
 app.listen(3000,()=>{
     console.log('server at http://127.0.0.1:3000');
 });
