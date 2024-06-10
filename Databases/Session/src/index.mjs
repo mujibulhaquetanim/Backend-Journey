3d277434e49f06ff6ee6d9c6ffb9ef26b1a155b8
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-06-10 22:57:34 +0600

    express-session added

--- Databases/Session/src/index.mjs
+++ Databases/Session/src/index.mjs
@@ -1,8 +1,14 @@
 import express from express;
+import session from 'express-session';
 import router from './routes/routes.mjs';
 
 const app = express();
 
+app.use(session({
+    secret: 'meow meow',
+    resave: false,
+    saveUninitialized: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
