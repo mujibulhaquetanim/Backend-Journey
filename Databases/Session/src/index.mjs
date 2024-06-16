9b27a5af0c53a68a1e8e26de860947b08ff1169c
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-06-16 21:50:54 +0600

    session added with cookie and console.logged session.id from req body

--- Databases/Session/src/index.mjs
+++ Databases/Session/src/index.mjs
@@ -1,18 +1,23 @@
 import express from 'express';
 import session from 'express-session';
 import router from './routes/routes.mjs';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(session({
     secret: 'meow meow',
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60000 * 60,
+    },
 }));
+app.use(cookieParser('sessionSauce'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
     res.send('Welcome to session server');
 })
 
--- Databases/Session/src/routes/routes.mjs
+++ Databases/Session/src/routes/routes.mjs
@@ -3,14 +3,17 @@ import { Router } from "express";
 const router = Router();
 
 router.route('/').get((req, res) => {
+    console.log(req.session);
+    console.log(req.session.id);
+    res.cookie('session', 'profile1', { maxAge: 60000, signed: true });
     res.send('Welcome to session route');
 });
 
-router.route('/profile').get((req, res) => {
+router.route('/profile').get((_, res) => {
     res.send('Welcome to profile route');
 });
 
-router.route('/logout').get((req, res) => {
+router.route('/logout').get((_, res) => {
     res.send('Welcome to logout route');
 });
 
