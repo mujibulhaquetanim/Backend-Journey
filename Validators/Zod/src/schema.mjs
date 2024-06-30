fb3a681f98eafecd0b69046c5536b45deefe2a05
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-07-01 00:07:18 +0600

    modified and fixed by exporting the schema

--- Validators/Zod/src/schema.mjs
+++ Validators/Zod/src/schema.mjs
@@ -6,7 +6,4 @@ const UserSchema = z.object({
     password: z.string().min(6),
 });
 
-User.parse({ username: "Ludwig" });
-
-type User = z.infer<typeof User>;
-console.log(User);
+export default UserSchema
