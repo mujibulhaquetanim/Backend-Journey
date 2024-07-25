11bdea54afcd02e68b7c622ba1c4003f712b70f5
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-07-25 23:59:53 +0600

    controller class added for redis crud operation

---
+++ Databases/Redis/src/CRUD/crud_Controller.mjs
@@ -0,0 +1,16 @@
+class CrudRedis {
+
+    constructor(connection) {
+        this.client = connection
+    }
+
+    //set key value
+
+    //get key value
+
+    //delete key value
+
+    //update key value
+
+    //expire key value
+}
