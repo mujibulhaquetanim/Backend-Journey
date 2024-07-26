2a7970a464c0b1d7dfa7d2debbb9f3ea5fe4fd53
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-07-26 23:58:13 +0600

    service for redisClient added

---
+++ Databases/Redis/src/CRUD/services/redisServices.mjs
@@ -0,0 +1,15 @@
+export class redisService{
+    constructor(client){
+        this.client = client
+    }
+
+    //set key value
+
+    //get key value
+
+    //delete key value
+
+    //update key value
+
+    //expire key value
+}
