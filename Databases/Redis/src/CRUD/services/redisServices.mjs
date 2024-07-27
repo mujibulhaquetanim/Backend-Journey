5634a8765b72cde90a3a948af84951529564d22d
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-07-27 23:40:19 +0600

    methods added for ping, server info, server config infos.

--- Databases/Redis/src/CRUD/services/redisServices.mjs
+++ Databases/Redis/src/CRUD/services/redisServices.mjs
@@ -3,6 +3,43 @@ export class redisService {
         this.client = client
     }
 
+    //ping redis server
+    async pingRedisServer() {
+        try {
+            return await this.client.ping()
+        } catch (error) {
+            throw new Error(`failed to ping redis server ${error}`)
+        }
+    }
+
+    //get redis server info
+    async getRedisServerInfo() {
+        try {
+            return await this.client.info()
+        } catch (error) {
+            throw new Error(`failed to get redis server info ${error}`)
+        }
+    }
+
+    //get redis server config
+    async getRedisServerConfig() {
+        try {
+            return await this.client.configGet()
+        } catch (error) {
+            throw new Error(`failed to get redis server config ${error}`)
+        }
+    }
+
+    //set redis server config
+    async setRedisServerConfig() {
+        try {
+            return await this.client.configSet()
+        } catch (error) {
+            throw new Error(`failed to set redis server config ${error}`)
+        }
+    }
+
+    //CRUD operations:
     //set key value
     async setKeyValue(key, value, expireTime) {
         try {
@@ -51,5 +88,4 @@ export class redisService {
             throw new Error(`failed to expire key ${key} with error ${error}`)
         }
     }
-
 }
