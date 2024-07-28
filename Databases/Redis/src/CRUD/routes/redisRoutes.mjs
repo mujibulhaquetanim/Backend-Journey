3dbb53dd8f8b6b971b01ab2cea1b0bec4210289c
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-07-28 23:29:26 +0600

    crud routes added for redis server

--- Databases/Redis/src/CRUD/routes/redisRoutes.mjs
+++ Databases/Redis/src/CRUD/routes/redisRoutes.mjs
@@ -1,15 +1,30 @@
 import { Router } from 'express';
-import redisConnection from '../db/redisConnect.mjs';
-import { redisService } from '../services/redisServices.mjs';
-import { CrudRedis } from '../controller/crud_Controller.mjs';
+import RedisConnection from '../utils/redisConnect.mjs';
+import { RedisService } from '../services/redisServices.mjs';
+import { CrudController } from '../controller/crud_Controller.mjs';
 
-const redis = new CrudRedis(redisConnection);
-
+const redisService = new redisService(RedisConnection);
+const redisController = new CrudController(RedisService);
 const router = Router();
 
 router.route('/').get((_, res) => {
     res.send('Redis CRUD Routes');
 })
+
+//set key value
+router.route('/set').post(redisController.setKeyValue);
+
+//get key value
+router.route('/get').get(redisController.getKeyValue);
+
+//delete key value
+router.route('/delete').delete(redisController.deleteKeyValue);
+
+//update key value
+router.route('/update').put(redisController.updateKeyValue);
+
+//expire key value
+router.route('/expire').post(redisController.expireKeyValue);
 
 
 export default router;
