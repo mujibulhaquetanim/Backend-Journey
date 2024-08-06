9176bd51e5a151d745a5c02ddbcd88f7fe459ff2
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-06 23:36:22 +0600

    type: module added, index file created for bulk import

---
+++ Databases/Postgres/CRUD Project/src/routes/index.js
@@ -0,0 +1,5 @@
+import { getBooks, getBookId } from './GetBooks.js'
+import { createCollection, deleteBooks, putBooks } from './PostBooks.js'
+import { deleteBooks, putBooks } from './Put_DeleteBooks.js'
+
+export { getBooks, getBookId, createCollection, deleteBooks, putBooks }
--- Databases/Postgres/CRUD Project/src/server.js
+++ Databases/Postgres/CRUD Project/src/server.js
@@ -1,7 +1,5 @@
-const express = require('express');
-const { getBooks, getBookId } = require('./routes/GetBooks');
-const { createCollection } = require('./routes/PostBooks');
-const { deleteBooks, putBooks } = require('./routes/Put_DeleteBooks');
+import express from 'express';
+import { getBooks, getBookId, createCollection, deleteBooks, putBooks } from './routes/index.js';
 
 const app = express();
 const PORT = 3001;
@@ -13,13 +11,13 @@ app.use(
 );
 app.use(express.json());
 
+app.get('/books', getBooks);
+app.get('/books/:id', getBookId);
+app.post('/books/', createCollection);
+app.delete('/books/:id', deleteBooks);
+app.put('/books/:id', putBooks);
+
 app.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:${PORT}`);
 
-})  
-
-app.get('/books',getBooks);
-app.get('/books/:id',getBookId);
-app.post('/books/',createCollection);
-app.delete('/books/:id',deleteBooks);
-app.put('/books/:id',putBooks);
+})
