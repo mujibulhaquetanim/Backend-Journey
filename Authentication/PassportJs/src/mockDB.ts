2763700d4c840121345a66baa32c7c1888ae67f3
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-22 23:43:45 +0600

    authenticate using MockDB and tryCatch added for error checking

---
+++ Authentication/PassportJs/src/mockDB.ts
@@ -0,0 +1,23 @@
+export const MockData = [
+    {
+        id:1,
+        name: "admin",
+        email: "admin@123.com",
+        password: "admin@123",
+        isAdmin: true
+    },
+    {
+        id:2,
+        name: "user",
+        email: "user@123.com",
+        password: "user@123",
+        isAdmin: false
+    },
+    {
+        id:3,
+        name: "user2",
+        email: "user2@123.com",
+        password: "user2@123",
+        isAdmin: false
+    }
+]
--- Authentication/PassportJs/src/strategy/local-strategy.ts
+++ Authentication/PassportJs/src/strategy/local-strategy.ts
@@ -1,8 +1,22 @@
 import passport from "passport";
 import { Strategy } from "passport-local";
+import { MockData } from "../mockDB";
 
+export default passport.use(new Strategy(
+  { },
+  (email: string, password: string, done: (error: any, user?: any, options?: { message: string }) => void) => {
+    console.log(`email: ${email}, password: ${password}`);
+    
+    try {
+      const user = MockData.find((user) => user?.email === email);
 
-passport.use(new Strategy({ usernameField: 'email', passwordField: 'password' }, (email, password, done) => {
-    console.log(email, password);
-    done(null, email);
-}))
+      if (!user) throw new Error("user not found");
+      if (user.password !== password) throw new Error("Invalid credentials");
+
+      done(null, user);
+
+    } catch (error) {
+      done(error, false);
+    }
+  }
+))
