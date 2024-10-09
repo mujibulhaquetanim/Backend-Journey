ddbf82981dc18c39ed230916646eb3a5654bafda
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-10-09 12:05:56 +0600

    generic error msg & proper email check added using zod's default email method in loginSchema

--- Authentication/JWT_Authentication/server/Validators/auth-validators.ts
+++ Authentication/JWT_Authentication/server/Validators/auth-validators.ts
@@ -17,18 +17,14 @@ const RegisterSchema = z.object({
     }).trim().min(6, 'Password must be at least 6 characters'),
     phone: z.string({
         required_error: 'Phone is required'
-    }).trim().min(10, 'Phone must be at least 10 characters')
+    }).trim().min(11, 'Phone must be at least 11 characters')
 });
 
 const LoginSchema = z.object({
-    email: z.string({
-        required_error: 'Email is required'
-    }).trim().email({
-        message: 'Invalid email'
-    }),
-    password: z.string({
-        required_error: 'Password is required'
-    }).trim().min(6, 'Password must be at least 6 characters')
+    email: z.string().email({
+        message: 'Invalid email format'
+    }).trim(),
+    password: z.string().trim().min(6, 'Invalid password')
 });
 
 export { RegisterSchema, LoginSchema };
