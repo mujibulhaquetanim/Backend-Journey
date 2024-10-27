716ab23e8c8214cbdc9b4559d918b59a699eed48
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-10-27 22:14:56 +0600

    new span element added into #app, script file added for that into index.html file & added css for that

--- APIs/trpc/ts-client/src/main.ts
+++ APIs/trpc/ts-client/src/main.ts
@@ -3,5 +3,6 @@ import './style.css'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Welcome to <span>TRPC</span> client</h1>
+    <h3>Hello route Output: <span id="hello"></span></h3>
   </div>
 `
