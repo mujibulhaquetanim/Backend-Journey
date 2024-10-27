37855590bf6b9b90e276d0c5468c5daaadb1ba82
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-10-27 22:09:45 +0600

    resolved cors error with the frontend with cors package & exported Types of AppRouter for using in the client side

--- APIs/trpc/express-server/src/index.ts
+++ APIs/trpc/express-server/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import { initTRPC } from "@trpc/server";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import cors from "cors";
+
 
 const app = express();
 const t = initTRPC.create();
@@ -16,8 +18,13 @@ const AppRouter = t.router({
     })
 })
 
+app.use(cors({
+    origin: "http://localhost:5173",
+}))
 app.use('/trpc', createExpressMiddleware({router: AppRouter}))
 
 app.listen(3000, () => {
     console.log(`server at http://localhost:3000`);
 })
+
+export type AppRouter = typeof AppRouter
