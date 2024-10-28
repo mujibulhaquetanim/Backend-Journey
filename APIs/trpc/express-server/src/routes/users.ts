9bedf2773b3bfb805c99b36034dc2b127cc92119
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-10-28 22:55:47 +0600

    output added after input to define what the mutation output should be, useful for more secure code practice

--- APIs/trpc/express-server/src/routes/users.ts
+++ APIs/trpc/express-server/src/routes/users.ts
@@ -10,13 +10,14 @@ export const UserRouter = t.router({
             name: "Emma Watson"
         }
     }),
-    updateUser: userProcedure.input(z.object({name: z.string()})).mutation((req)=>{
+    updateUser: userProcedure.input(z.object({name: z.string()})).output(z.object({id: z.string(), name: z.string()})).mutation((req)=>{
         console.log(`
             updated user ${req.input.userId} with name: ${req.input.name}
             `);
             return {
-                id: req.input.userId,
-                name: req.input.name
+                id: req.input.userId, //required if not then it will throw an error
+                name: req.input.name, //required if not then it will throw an error
+                password: "password" //it won't be stored as it is not in the output schema, which is good for security.
             }
     })
         
