c83a674658148e4a09496bda0476a1bc477e799c
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-11-19 23:13:01 +0600

    magicLink stategy setup added using passportjs

--- NestJs/Auth/passwordless/src/auth/auth.controller.ts
+++ NestJs/Auth/passwordless/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -6,7 +6,15 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {
 
     //POST /api/auth/login {email} -> send magic link
+    @Post('/api/auth/login')
+    login(){
+      //
+    }
 
     //GET /api/auth/callback?token=token=some-token -> JWT access token
+    @Get('/api/auth/callback')
+    callback(){
+      //
+    }
   }
 }
---
+++ NestJs/Auth/passwordless/src/auth/magicLogin.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable, Logger } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { verify } from "crypto";
+import Strategy from "passport-magic-login"
+import { AuthService } from "./auth.service";
+
+@Injectable()
+export class MagicLoginStrategy extends PassportStrategy(Strategy) {
+
+    //instead of using SMTP,i am using a logger to send the magic link
+    private readonly logger = new Logger(MagicLoginStrategy.name);
+    constructor(private authService: AuthService){
+        super({
+            secret: "aayin",
+            jwtOptions: {expiresIn: "20m"},
+            callbackUrl: "http://localhost:3000/api/auth/callback",
+            sendMagicLink: (email, link) => this.logger.debug(`Magic link sent to ${email}: ${link}`),
+            verify: async (payload, callback) => {
+                callback(null, this.validate(payload))
+            }
+        })
+    }
+
+    validate(payload){
+        //validate email
+    }
+}
