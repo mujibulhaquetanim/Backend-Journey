8323a4d30758cd749e99cd623c60a20205012f15
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-11-20 13:05:34 +0600

    validateUser logic added to the authService to check wether given destination/email valid or not

--- NestJs/Auth/passwordless/src/auth/auth.module.ts
+++ NestJs/Auth/passwordless/src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { MagicLoginStrategy } from './magicLogin.strategy';
+import { UsersService } from 'src/users/users.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [UsersModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, MagicLoginStrategy],
 })
 export class AuthModule {}
--- NestJs/Auth/passwordless/src/auth/auth.service.ts
+++ NestJs/Auth/passwordless/src/auth/auth.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly userService: UsersService){}
+  validateUser(email: string) {
+    const user= this.userService.findOneByEmail(email);
+
+    if(!user) throw new UnauthorizedException();
+
+    return user;
+  }
+}
