e5e6a57ab81eeeff4c16d21becc18ec5aa5c45af
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-11-22 23:01:01 +0600

    [passwordless] @nestjs/config package added to handle .env variables

--- NestJs/Auth/passwordless/src/auth/strategy/magicLogin.strategy.ts
+++ NestJs/Auth/passwordless/src/auth/strategy/magicLogin.strategy.ts
@@ -2,15 +2,16 @@ import { Injectable, Logger } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import Strategy from "passport-magic-login"
 import { AuthService } from "../auth.service";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class MagicLoginStrategy extends PassportStrategy(Strategy) {
     //instead of using SMTP,i am using a logger to send the magic link
     private readonly logger = new Logger(MagicLoginStrategy.name);
 
-    constructor(private authService: AuthService){
+    constructor(private authService: AuthService, private readonly configService: ConfigService){
         super({
-            secret: "aayin",
+            secret: configService.get<string>("STRATEGY_SECRET"),
             jwtOptions: {expiresIn: "20m"},
             callbackUrl: "http://localhost:3000/api/auth/login/callback",
             sendMagicLink: async (destination, href) => this.logger.debug(`Magic link sent to ${destination}: ${href}`),
