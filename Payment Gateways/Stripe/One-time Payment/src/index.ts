b9c2e30ed37c10bac97258f4281105688cb83ea5
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-11-24 23:38:26 +0600

    cors, dotenv added & server file added + client->index.ts

--- Payment Gateways/Stripe/One-time Payment/src/index.ts
+++ Payment Gateways/Stripe/One-time Payment/src/index.ts
@@ -1,16 +1,26 @@
 import express, { Request, Response } from "express";
 import Stripe from "stripe";
-
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config({
+  path: './env'
+});
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
 
-const stripe = new Stripe(STRIPE_SECRET_KEY!)
-
+const stripe = new Stripe(STRIPE_SECRET_KEY!);
+app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript with Express!");
+app.get("/create-checkout-intent", async(req: Request, res: Response) => {
+  try {
+    const {items}= req.body;
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 app.listen(PORT, () => {
