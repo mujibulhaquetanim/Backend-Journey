aba722eb43282ab2967ab0858bb6b657c78a3ebd
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-11-24 14:43:48 +0600

    @nestjs/swagger used for api documentations + added authentication for too

--- NestJs/Auth/passwordless/src/main.ts
+++ NestJs/Auth/passwordless/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder().setTitle('Passwordless API').setDescription('Authenticate with magic link').addTag("MagicLinkLogin").setVersion('1.0').addBearerAuth().build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`Application is running on: ${await app.getUrl()}/api`);
 }
 bootstrap();
