69241798047c123f26a424e5dd650ee3205cb1cd
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-11-24 14:45:29 +0600

    @ApiProperty of swagger added for showing schema in the swagger ui + some debugging help (console.log) removed.

--- NestJs/Auth/passwordless/src/auth/dto/login.dto.ts
+++ NestJs/Auth/passwordless/src/auth/dto/login.dto.ts
@@ -1,6 +1,11 @@
 import {IsEmail} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class loginDto{
     @IsEmail()
+    @ApiProperty({
+        description: "Example Email",
+        example: "dummy1@gmail.com"
+    })
     destination: string;
 }
