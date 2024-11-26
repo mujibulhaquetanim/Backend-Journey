5588adf1f015276410962575b886dd80fb58a475
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-11-26 20:34:10 +0600

    optimization added to remove unnecessary code with webpack + stripe new key added for the frontend using dotenv

--- Payment Gateways/Stripe/One-time Payment/webpack.config.cjs
+++ Payment Gateways/Stripe/One-time Payment/webpack.config.cjs
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+require("dotenv").config();
 
 module.exports = {
   entry: "./src/Client.ts", // Your entry file
@@ -19,10 +20,13 @@ module.exports = {
       },
     ],
   },
+  optimization: {
+    usedExports: true,
+  },
   plugins: [
     new webpack.DefinePlugin({
       "process.env": JSON.stringify({
-        STRIPE_API_KEY: process.env.STRIPE_API_KEY, // Inject Stripe API key or other env vars
+        STRIPE_Publishable_KEY: process.env.STRIPE_Publishable_KEY, // Inject Stripe API key or other env vars
       }),
     }),
   ],
