a7c69a8b0c631f9e3dcde5ef0c75ee8b8fee15de
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-12-24 11:58:43 +0600

    necessary notes added for express server functionalities

--- Validators/Zod/src/index.ts
+++ Validators/Zod/src/index.ts
@@ -20,9 +20,11 @@ app.post(
     }
 );
 
-// Error-handling middleware
+// Error-handling middleware function that takes four arguments. This function will be called if an error occurs in the route handler or in any of the middleware functions that are called before this one.
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    // err.stack will contain the stack trace of the error. stack trace is a list of function calls that were in progress when the error occurred.
     console.error(err.stack);
+    // check if the response headers have already been sent. If they have, call the next middleware in the chain with the error. If not, send a 500 response with a JSON object containing the message "Internal Server Error". If we don't check headersSent, the server will crash with an unhandled promise rejection error. This is because the response headers can only be sent once, and if we try to send them again, the server will throw an error. headersSent is a boolean property of the response object that indicates whether the response headers have been sent. If the headers have been sent, it will be true; otherwise, it will be false. We can use this property to check if the headers have been sent before sending a response. it is a good practice to check if the headers have been sent before sending a response to avoid errors.
     if (res.headersSent) {
         return next(err);
     }
