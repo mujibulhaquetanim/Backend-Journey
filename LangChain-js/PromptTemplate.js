89c266800f14824221099c30942bc08389cfe17c
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-02-15 22:47:58 +0600

    given input to the llm using template and placeholder added which will be filled with customized parameters.

---
+++ LangChain-js/PromptTemplate.js
@@ -0,0 +1,30 @@
+import { configDotenv } from "dotenv";
+import { ChatPromptTemplate } from '@langchain/core/prompts';
+import { ChatGroq } from "@langchain/groq";
+
+configDotenv();
+
+const model = new ChatGroq({
+    apiKey: process.env.GROQ_API_KEY,
+    model: "qwen-2.5-32b",
+    temperature: 0.7
+});
+
+// template string to create a prompt which will be passed to the model. the placeholder {tone}, {company}, {position}, and {skill} will be replaced with the values passed to the prompt.
+const template = "Write a {tone} email to {company} expressing interest in the {position} position, mentioning {skill}";
+
+const promptTemplate = ChatPromptTemplate.fromTemplate(template);
+
+// format returns a string
+const prompt = await promptTemplate.format({
+    tone: "formal",
+    company: "Google",
+    position: "Software Engineer",
+    skill: "JavaScript"
+})
+
+console.log("Type of prompt:", typeof prompt);
+console.log("Prompt:", prompt);
+
+const response = await model.invoke(prompt);
+console.log(response);
