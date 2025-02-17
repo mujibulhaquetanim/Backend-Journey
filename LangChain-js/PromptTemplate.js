f5aa0edafa1c6637e4b4716bdb8a57d9c4f7d259
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-02-17 22:19:05 +0600

    generated data organized in a directory.

--- LangChain-js/PromptTemplate.js
+++ LangChain-js/PromptTemplate.js
@@ -12,17 +12,17 @@ const model = new ChatGroq({
 });
 
 // template string to create a prompt which will be passed to the model. the placeholder {tone}, {company}, {position}, and {skill} will be replaced with the values passed to the prompt.
-const template = "Write a {tone} email to {company} expressing interest in the {position} position, mentioning {skill}";
+// const template = "Write a {tone} email to {company} expressing interest in the {position} position, mentioning {skill}";
 
-const promptTemplate = ChatPromptTemplate.fromTemplate(template);
+// const promptTemplate = ChatPromptTemplate.fromTemplate(template);
 
-// invoke returns an object
-const prompt = await promptTemplate.invoke({
-    tone: "formal",
-    company: "Google",
-    position: "Software Engineer",
-    skill: "JavaScript"
-});
+// // invoke returns an object
+// const prompt = await promptTemplate.invoke({
+//     tone: "formal",
+//     company: "Google",
+//     position: "Software Engineer",
+//     skill: "JavaScript"
+// });
 
 // format returns a string
 // const prompt = await promptTemplate.format({
@@ -32,8 +32,13 @@ const prompt = await promptTemplate.invoke({
 //     skill: "JavaScript"
 // })
 
-console.log("Type of prompt:", typeof prompt);
-console.log("Prompt:", prompt);
+// console.log("Type of prompt:", typeof prompt);
+// console.log("Prompt:", prompt);
+
+const message = [
+    {"system": "You are a senior AI Engineer who resolve {topic}"},
+    {"human": "Help me about {question}"}
+]
 
 const response = await model.invoke(prompt);
 console.log(response.content);
