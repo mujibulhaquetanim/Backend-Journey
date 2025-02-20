5291723d9bde24177b788b72cb921aaf2816d815
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-02-20 23:21:11 +0600

    implemented chaining with runnableLamda funciton and RunnableSequence of runnables package of langchain

---
+++ LangChain-js/src/ChainingWithoutPipe.js
@@ -0,0 +1,38 @@
+import { configDotenv } from "dotenv"
+import { ChatGroq } from "@langchain/groq"
+import { ChatPromptTemplate } from "@langchain/core/prompts"
+import { RunnableLambda, RunnableSequence } from "@langchain/core/runnables"
+
+configDotenv();
+
+const model = new ChatGroq({
+    apiKey: process.env.GROQ_API_KEY,
+    model: "qwen-2.5-32b",
+    temperature: 0.7
+});
+
+const promptTemplate = ChatPromptTemplate.fromMessages([
+    ["system", "You love facts and you tell facts about {animal}"],
+    ["user", "Tell me {count} facts."]
+  ]);
+
+const formatPrompt = new RunnableLambda({
+    func: (input) => promptTemplate.formatPromptValue(input)
+  });
+  
+  const invokeModel = new RunnableLambda({
+    func: (promptValue) => model.invoke(promptValue)
+  });
+  
+  const parseOutput = new RunnableLambda({
+    func: (response) => response.content
+  });
+  
+  const chain = RunnableSequence.from([formatPrompt, invokeModel, parseOutput]);
+  
+  async function main() {
+    const response = await chain.invoke({ animal: "cat", count: 2 });
+    console.log(response);
+  }
+  
+  main();
