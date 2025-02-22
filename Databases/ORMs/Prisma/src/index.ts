4e75dd6def5904f7dabd542dbfdb9b1065d3715d
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-02-22 23:34:11 +0600

    check user in the db and for exiting it will update none otherwise create the user using one single prisma command. `upsert`.

--- Databases/ORMs/Prisma/src/index.ts
+++ Databases/ORMs/Prisma/src/index.ts
@@ -3,27 +3,40 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main(){
-    //existing user check
-    const user = await prisma.user.findUnique({
-        where: {
-            email: 'mujibai@me.com'
-        }
-    });
+    // //existing user check
+    // const user = await prisma.user.findUnique({
+    //     where: {
+    //         email: 'mujibai@me.com'
+    //     }
+    // });
 
-    if(user){
-        console.log("User already exists");
-        return
-    }
+    // if(user){
+    //     console.log("User already exists");
+    //     return
+    // }
 
-    //create new user
-    const newUser = await prisma.user.create({
-        data:{
+    // //create new user
+    // const newUser = await prisma.user.create({
+    //     data:{
+    //         name: "mujibai",
+    //         email: "mujibai@me.com",
+    //         password: "baigan",
+    //         joinedAt: new Date(),
+    //     }
+    // })
+
+    //method 2 for checking existing user and creating new user
+    const user = await prisma.user.upsert({
+        where: {email: "mujibai@me.com"},
+        update: {},
+        create: {
             name: "mujibai",
             email: "mujibai@me.com",
             password: "baigan",
             joinedAt: new Date(),
         }
     })
+    console.log('user: ',user);
 
 }
 
