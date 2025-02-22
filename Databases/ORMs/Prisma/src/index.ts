60b488c24b07ad4d4b1fb70c2335e4a99bdc9913
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-02-22 23:28:39 +0600

    existing user check added so that it doesn't throw error for existing error.

--- Databases/ORMs/Prisma/src/index.ts
+++ Databases/ORMs/Prisma/src/index.ts
@@ -3,6 +3,18 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main(){
+    //existing user check
+    const user = await prisma.user.findUnique({
+        where: {
+            email: 'mujibai@me.com'
+        }
+    });
+
+    if(user){
+        console.log("User already exists");
+        return
+    }
+
     //create new user
     const newUser = await prisma.user.create({
         data:{
