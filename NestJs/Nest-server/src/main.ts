ebd776d0f489cd7afb2c1ef44d1a668e4115540e
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-02-26 23:49:48 +0600

    interceptor to intercept req and before sending to the handler and to the client added in the globalscope of the nest-server

--- NestJs/Nest-server/src/main.ts
+++ NestJs/Nest-server/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggerInterceptor } from './interceptors/logger/logger.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new LoggerInterceptor());
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
