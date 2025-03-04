ed204a4a3da93749405983f1d00535f50106a6d9
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-03-04 23:50:54 +0600

    implement mergeMap example in the `of` operator pipeline

--- rxJs/src/app.service.ts
+++ rxJs/src/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { filter, map, Observable, of, tap } from 'rxjs';
+import { filter, map, mergeMap, Observable, of, tap } from 'rxjs';
 
 @Injectable()
 export class AppService {
@@ -15,7 +15,8 @@ export class AppService {
     // of is observable which takes any number of arguments and emits them one by one in sequence, it converts plain data into stream.
     // using tap function we can add a side effect to the observable without changing its value.
     //map function that transforms the value emitted by the observable
-    of(1,3,5,6,7).pipe(tap((data)=> console.log(`before ${data}`)), filter((data)=> data%2===0), map((data)=> data*2), tap((data)=> console.log(`after ${data}`))).subscribe();
+    //Maps each transformed number to a new observable that emits a string.
+    of(1,3,5,6,7).pipe(tap((data)=> console.log(`before ${data}`)), filter((data)=> data%2===0), map((data)=> data*2), mergeMap((id)=> of(`hello ${id}`)), tap((data)=> console.log(`after ${data}`))).subscribe();
 
     return 'Welcome to RxJS';
   }
