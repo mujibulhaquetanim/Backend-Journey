06e082476ac2bfd7977aa0938dfc9b20b9065a95
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-03-07 23:59:07 +0600

    drizzle schemas organized in new schema dir

---
+++ Databases/ORMs/Drizzle/src/db/schema/schema.ts
@@ -0,0 +1,2 @@
+import * as users from './users.schema';
+import * as profileInfo from './profileInfo.schema';
--- Databases/ORMs/Drizzle/src/db/schema/users.schema.ts
+++ Databases/ORMs/Drizzle/src/db/schema/users.schema.ts
@@ -1,4 +1,4 @@
-import { integer, jsonb, pgTable, serial, varchar } from "drizzle-orm/pg-core";
+import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
@@ -6,11 +6,4 @@ export const users = pgTable('users', {
   lastName: varchar("lname", {length: 32}).notNull(),
   email: varchar("email", {length: 32}).notNull(),
   password: varchar("password", {length: 32}).notNull(),
-})
-
-
-export const profileInfo = pgTable('profileInfo', {
-  id: serial('id').primaryKey(),
-  metadata: jsonb("metadata"),
-  userId: integer("userId").references(()=> users.id)
 })
