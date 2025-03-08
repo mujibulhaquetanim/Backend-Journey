746a411e90f2e745670a1f7197ee057e60de02b1
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-03-08 23:50:47 +0600

    implement many to many relationship with join table

--- Databases/ORMs/Drizzle/src/db/schema/groups.schema.ts
+++ Databases/ORMs/Drizzle/src/db/schema/groups.schema.ts
@@ -1,6 +1,20 @@
 import { integer, pgTable, primaryKey, serial, text } from "drizzle-orm/pg-core";
+import { users } from "./users.schema";
 
 export const groups = pgTable('groups', {
     id: serial('id').primaryKey(),
     name: text('name').notNull(),
 })
+
+
+//join table, here instead of primary key, we use composite key because one user can be in multiple groups.
+//many to many relationship, we need to create a join table between users and groups.
+export const groupsToUsers = pgTable('groupsToUsers', {
+    userId: integer('userId').references(()=> users.id),
+    groupId: integer('groupId').references(()=> groups.id)
+},
+
+(table)=>({
+    pk: primaryKey({columns: [table.userId, table.groupId]})
+})
+)
