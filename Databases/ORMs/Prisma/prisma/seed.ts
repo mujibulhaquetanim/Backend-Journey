9890fd82278b9a85b8ec0f349aaacc1633f31813
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-03-10 23:27:36 +0600

    seed circular friendship with 10 of the users using modulo operator

--- Databases/ORMs/Prisma/prisma/seed.ts
+++ Databases/ORMs/Prisma/prisma/seed.ts
@@ -3,6 +3,13 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
+// Clear existing data to avoid unique constraint conflicts as the seed script is run multiple times
+  await prisma.friendship.deleteMany({});
+  await prisma.post.deleteMany({});
+  await prisma.profile.deleteMany({});
+  await prisma.user.deleteMany({});
+  await prisma.jobTitle.deleteMany({});
+  await prisma.category.deleteMany
   // Seed Users
   for (let i = 1; i <= 10; i++) {
     const user = await prisma.user.create({
@@ -31,8 +38,25 @@ async function main() {
         updatedAt: new Date(),
       },
     });
+
+    // Seed Friendships
+    //modulo operator for creating circular friendships i.e. Users 1 to 9 become friends with the next user (e.g., User 1 with User 2, User 2 with User 3, and so on). The last user (User 10) becomes friends with the first user (User 1), creating a circular friendship.
+
+    const friend = await prisma.user.findUnique({
+        where: { email: `user${(i % 10) + 1}@example.com` },
+      });
+      if (friend) {
+        await prisma.friendship.create({
+          data: {
+            userId: user.id,
+            friendId: friend.id,
+          },
+        });
+      }
+    }
+
   }
-}
+
 
 main()
   .catch(e => {
